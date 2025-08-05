26f6e6191be96d96a9aeb39b3d9a304c202a2559
jhlee-codes <jhleezz00@naver.com>
2025-08-05 10:11:28 +0900

    feat: CSRF 보호 설정 적용

--- src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -2,19 +2,33 @@ package com.sprint.mission.discodeit.config;
 
 import java.util.List;
 import java.util.stream.IntStream;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        log.debug("[SecurityConfig] FilterChain 구성 시작");
+
+        http
+            // CSRF 설정
+            .csrf(csrf -> csrf
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler())
+            );
+
         return http.build();
     }
 
@@ -28,8 +42,8 @@ public class SecurityConfig {
                     filterChain.getFilters().get(idx).getClass()))
                 .toList();
 
-            System.out.println("현재 적용된 필터 체인 목록:");
-            filterNames.forEach(System.out::println);
+            log.debug("현재 적용된 필터 체인 목록:");
+            filterNames.forEach(log::debug);
         };
     }
 }
--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -6,19 +6,34 @@ import com.sprint.mission.discodeit.dto.User.UserDto;
 import com.sprint.mission.discodeit.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
 @RestController
 public class AuthController implements AuthApi {
 
     private final AuthService authService;
+
+    @GetMapping("csrf-token")
+    public ResponseEntity<Void> getCsrfToken(CsrfToken csrfToken) {
+        String tokenValue = csrfToken.getToken();
+        log.debug("CSRF 토큰 요청: {}", tokenValue);
+
+        return ResponseEntity
+            .status(HttpStatus.NO_CONTENT)
+            .build();
+    }
 
     /**
      * 사용자 로그인 인증
@@ -29,12 +44,12 @@ public class AuthController implements AuthApi {
     @PostMapping(path = "/login")
     @Override
     public ResponseEntity<UserDto> login(
-            @Valid @RequestBody LoginRequest loginRequest
+        @Valid @RequestBody LoginRequest loginRequest
     ) {
         UserDto authUser = authService.login(loginRequest);
 
         return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(authUser);
+            .status(HttpStatus.OK)
+            .body(authUser);
     }
 }
