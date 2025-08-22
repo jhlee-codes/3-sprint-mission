6af401e0f2b01eaa01a4199627e94220a8a85b96
jhlee-codes <jhleezz00@naver.com>
2025-08-22 13:12:56 +0900

    fix: 만료된 토큰 삭제시 만료된 토큰 검증 보완

--- src/main/java/com/sprint/mission/discodeit/auth/jwt/store/InMemoryJwtRegistry.java
+++ src/main/java/com/sprint/mission/discodeit/auth/jwt/store/InMemoryJwtRegistry.java
@@ -128,8 +128,8 @@ public class InMemoryJwtRegistry implements JwtRegistry {
             Queue<JwtInformation> queue = entry.getValue();
             queue.removeIf(jwtInformation -> {
                 boolean isExpired =
-                    jwtTokenProvider.validateAccessToken(jwtInformation.getAccessToken()) ||
-                        jwtTokenProvider.validateRefreshToken(jwtInformation.getRefreshToken());
+                    !jwtTokenProvider.validateAccessToken(jwtInformation.getAccessToken()) ||
+                        !jwtTokenProvider.validateRefreshToken(jwtInformation.getRefreshToken());
                 if (isExpired) {
                     removeTokenIndex(
                         jwtInformation.getAccessToken(),
