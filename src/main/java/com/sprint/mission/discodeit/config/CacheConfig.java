2157958b75e5658310810228e553b17e521b5027
jhlee-codes <jhleezz00@naver.com>
2025-09-03 11:17:50 +0900

    refactor: Caffeine 캐시 매니저 등록 빈 삭제

--- src/main/java/com/sprint/mission/discodeit/config/CacheConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/CacheConfig.java
@@ -4,13 +4,10 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectMapper.DefaultTyping;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
-import com.github.benmanes.caffeine.cache.Caffeine;
 import java.time.Duration;
-import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -34,38 +31,6 @@ public class CacheConfig {
         return RedisCacheManager.builder(cf)
             .cacheDefaults(redisCfg)
             .build();
-    }
-
-    @Bean("caffeineCacheManager")
-    public CacheManager cacheManager() {
-        CaffeineCacheManager manager = new CaffeineCacheManager();
-
-        manager.setCaffeine(Caffeine.newBuilder()
-            .maximumSize(100)
-            .expireAfterAccess(Duration.ofMinutes(10))
-            .recordStats()
-            .removalListener((key, value, cause) -> {
-                switch (cause) {
-                    case SIZE:
-                        log.debug("캐시 크기 초과로 인한 엔트리 제거 - key: {}", key);
-                        break;
-                    case EXPIRED:
-                        log.debug("캐시 만료로 인한 엔트리 제거 - key: {}", key);
-                        break;
-                    case EXPLICIT:
-                        log.info("캐시 수동 삭제로 인한 엔트리 제거 - key: {}", key);
-                        break;
-                    case REPLACED:
-                        log.debug("캐시 새 값으로 교체로 인한 엔트리 제거 - key: {}", key);
-                        break;
-                    default:
-                        log.debug("캐시 엔트리 제거 - key: {}, cause: {}", key, cause);
-                }
-            })
-        );
-
-        manager.setCacheNames(List.of("user:channels", "user:notifications", "users:list"));
-        return manager;
     }
 
     @Bean
